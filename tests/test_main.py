4879977e7e534613c79ade10d7a01846b4e0f5bd
Mustafa <mustafa@Mustafa-MacBook-Air.local>
2026-05-24 08:24:06 +0300

    fix: rewrite the code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(file))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi.testclient import TestClient
 from main import app
